e908e0c97c3c7556546eb39918e880b8779e0dcf
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2018-02-19 19:20:53 +0800

    update

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,10 @@
 wget http://www.ipdeny.com/ipblocks/data/countries/cn.zone -O cn.acl
-cat ad.acl lan.acl cn.acl >my.acl
+cat ad.acl lan.acl zl.acl cn.acl >my.acl
 cp my.acl ACL/my.acl
 cp ad.acl ACL/ad.acl
 cp lan.acl ACL/lan.acl
 cp cn.acl ACL/cn.acl
+cp zl.acl ACL/zl.acl
 cp gitpush.sh ACL/gitpush.sh
 cp 抓包列表.txt ACL/抓包列表.txt
 cp README.md ACL/README.md
