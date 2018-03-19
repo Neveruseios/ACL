a2c4dd630ad43bf86633087c139b2503d199d7d3
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2018-03-19 12:14:26 +0800

    update

--- gitpush.sh
+++ gitpush.sh
@@ -4,22 +4,20 @@ git fetch origin master:temp
 git diff temp
 git merge temp
 git branch -d temp
-cp zl.acl /mnt/d/code/ACL/zltemp.acl
-cp myad.acl /mnt/d/code/ACL/myadtemp.acl
 cd ..
-cat zl.acl zltemp.acl | sort | uniq > zl.acl
-cat myad.acl myadtemp.acl | sort | uniq > myad.acl
-rm -rf zltemp.acl myadtemp.acl
 wget http://www.ipdeny.com/ipblocks/data/countries/cn.zone -O cn.acl
-cat adstart.acl myad.acl ad.acl zlstart.acl lan.acl zl.acl cn.acl >my.acl
+cat start.acl adstart.acl myad.acl ad.acl zlstart.acl lan.acl zl.acl cn.acl >my.acl
+cat start.acl zlstart.acl lan.acl zl.acl cn.acl >my-none.acl
+cp start.acl ACL/start.acl
+cp my-none.acl ACL/my-none.acl
+cp zlstart.acl ACL/zlstart.acl
+cp myad.acl ACL/myad.acl
+cp adstart.acl ACL/adstart.acl
 cp my.acl ACL/my.acl
 cp ad.acl ACL/ad.acl
 cp lan.acl ACL/lan.acl
 cp cn.acl ACL/cn.acl
 cp zl.acl ACL/zl.acl
-cp zlstart.acl ACL/zlstart.acl
-cp myad.acl ACL/myad.acl
-cp adstart.acl ACL/adstart.acl
 cp gitpush.sh ACL/gitpush.sh
 cp 抓包列表.txt ACL/抓包列表.txt
 cp README.md ACL/README.md
@@ -27,4 +25,5 @@ cd ACL
 git add .
 git commit -m "update"
 git push -u origin master
-cd ..
+cd
+~          
