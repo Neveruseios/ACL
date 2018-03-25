2765637241fee707227afdcff397e7c428b4f021
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2018-03-25 09:35:04 +0800

    update

--- gitpush.sh
+++ gitpush.sh
@@ -6,7 +6,17 @@ git merge temp
 git branch -d temp
 cd ..
 wget http://www.ipdeny.com/ipblocks/data/countries/cn.zone -O cn.acl
-cat start.acl adstart.acl myad.acl ad.acl zlstart.acl lan.acl zl.acl cn.acl >my.acl
+wget https://neko-dev.github.io/neohosts/full/hosts -O neohosts.acl
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts -O stevenhosts.acl
+sed -i 's/\./\\\./g' neohosts.acl
+sed -i 's/$/\$/g' neohosts.acl
+sed -i 's/0\\.0\\.0\\.0 /(^|\\.)/' neohosts.acl
+sed -i '1,14d' neohosts.acl
+sed -i 's/\./\\\./g' stevenhosts.acl
+sed -i 's/$/\$/g' stevenhosts.acl
+sed -i 's/0\\.0\\.0\\.0 /(^|\\.)/' stevenhosts.acl
+sed -i '1,31d' stevenhosts.acl
+cat start.acl adstart.acl neohosts.acl stevenhosts.acl myad.acl ad.acl zlstart.acl lan.acl zl.acl cn.acl >my.acl
 cat start.acl zlstart.acl lan.acl zl.acl cn.acl >my-none.acl
 cp start.acl ACL/start.acl
 cp my-none.acl ACL/my-none.acl
