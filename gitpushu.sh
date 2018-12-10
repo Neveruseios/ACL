344a7056dd73c2186137915069a86d932787b47e
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2018-12-10 20:53:13 +0800

    update

--- gitpushu.sh
+++ gitpushu.sh
@@ -17,10 +17,13 @@ cd ..
 cp cn/cn.acl cn.acl
 cp ad/ad.acl ad.acl
 cp ad/my_ad.acl my_ad.acl
+cp proxy/gfwlist.acl gfwlist.acl
 
-cat start.acl ad.acl zlstart.acl lan.acl zl.acl cn.acl >my.acl
-cat start.acl my_ad.acl zlstart.acl lan.acl zl.acl cn.acl >my-light.acl
-cat start.acl zlstart.acl lan.acl zl.acl cn.acl >my-none.acl
+cat start.acl ad.acl zlstart.acl lan.acl zl.acl cn.acl gfwlist.acl>my.acl
+cat start.acl my_ad.acl zlstart.acl lan.acl zl.acl cn.acl gfwlist.acl>my-light.acl
+cat start.acl zlstart.acl lan.acl zl.acl cn.acl gfwlist.acl>my-none.acl
+
+rm cn.acl ad.acl my_ad.acl gfwlist.acl
 
 sed -i -r '/^#/d'  my.acl
 sed -i -r '/^#/d'  my-none.acl
@@ -31,6 +34,7 @@ sed -i -r '/^$/d' my-none.acl
 
 cp -frap cn/ ACL/
 cp -frap ad/ ACL/
+cp -frap proxy/ ACL/
 cp start.acl ACL/start.acl
 cp my-none.acl ACL/my-none.acl
 cp my-light.acl ACL/my-light.acl
