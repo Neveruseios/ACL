6c45a566447d0db485d5d4ab2392b51893332ca9
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2018-12-14 15:53:47 +0800

    update

--- cn/get_cn.sh
+++ cn/get_cn.sh
@@ -1,9 +1,8 @@
 proxychains wget http://geolite.maxmind.com/download/geoip/database/GeoIPCountryCSV.zip -O GeoIPCountryCSV.zip
 proxychains wget http://www.ipdeny.com/ipblocks/data/countries/cn.zone -O cnic.txt
 unzip -o GeoIPCountryCSV.zip
-perl geo2nginx.pl < GeoIPCountryWhois.csv > nginx_geoip.txt
-cat nginx_geoip.txt | grep CN | sed 's/ CN;//g' > geoipCN.txt
+perl geo2nginx.pl < GeoIPCountryWhois.csv | grep CN | sed 's/ CN;//g' > geoipCN.txt
 #cat nginx_geoip.txt | grep CN > geoipCN_temp.txt
 #python ipsort.py
 cat cnic.txt geoipCN.txt |uniq > cn.acl
-rm geoipCN.txt nginx_geoip.txt GeoIPCountryCSV.zip
+rm geoipCN.txt GeoIPCountryCSV.zip GeoIPCountryWhois.csv
