895537592d3aedad5c15ee5b058cae5196382ed9
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2018-12-22 23:54:17 +0800

    update

--- ad/ad.sh
+++ ad/ad.sh
@@ -6,7 +6,7 @@ cat pglyoyo.acl neohosts.acl stevenhosts.acl   | sed 's/^\#.*//g' | sed '/^ *$/d
 sed 's/\./\\\./g' | sed 's/$/\$/g' | sed 's/^\:\:1.*//g' | sed 's/^fe[0-9][0-9]\:\:.*//g' | \
 sed 's/^ff[0-9][0-9]\:\:.*//g' | sed 's/^255\.255\.255\.255.*//g' |sed 's/127\\.0\\.0\\.1 /(^|\\.)/' | \
 sed 's/0\\.0\\.0\\.0 /(^|\\.)/' | sed 's/\:\: /(^|\\.)/' | sed 's/\n\$/\$/' | \
-sed 's///g' | uniq  > ad_temp.acl
+uniq  > ad_temp.acl
 
 #sort ad_temp.acl | uniq  | sed 's/^\#.*//g' | sed '/^ *$/d' | sed '/^$/d' | \
 #sed 's/\./\\\./g' | sed 's/$/\$/g' | sed 's/0\\.0\\.0\\.0 /(^|\\.)/' | \
@@ -33,7 +33,7 @@ sed 's///g' | uniq  > ad_temp.acl
 #sed -i 's/127\\.0\\.0\\.1 /(^|\\.)/' pglyoyo.acl
 #sed -i '1,13d' pglyoyo.acl
 
-cat adstart.acl myad.acl  ad_temp.acl > ad.acl
+cat adstart.acl myad.acl  ad_temp.acl | sed 's///g' > ad.acl
 cat adstart.acl myad.acl > light_ad.acl
 rm ad_temp.acl pglyoyo.acl stevenhosts.acl neohosts.acl
 
