1decebf1af7c54084bf0308eb69749dc0624d7b7
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-02-19 15:44:17 +0800

    update

--- cn/get_cn.sh
+++ cn/get_cn.sh
@@ -1,10 +1,11 @@
 wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip -O GeoIPCountryCSV.zip
 wget http://www.ipdeny.com/ipblocks/data/countries/cn.zone -O cnic.txt
+wget http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -O apnic.txt
 unzip GeoIPCountryCSV.zip
 cd GeoLite2-Country-CSV*
 cat GeoLite2-Country-Blocks-IPv4.csv | grep 1814991 | cut -d ',' '-f1' > ../geoipCN.txt
 cd ..
-cat cnic.txt geoipCN.txt |uniq > cn_tmp.acl
-cat cn_site.acl cn_tmp.acl > cn.acl
-rm geoipCN.txt GeoIPCountryCSV.zip cnic.txt cn_tmp.acl
+cat apnic.txt  | cut -d '|' -f2,4,5 | grep CN | cut -d '|' -f2,3 | sed "s/|/\//g" | sed "/^[0-9]*\/[0-9]*/d" > apnic_cn.txt
+cat cnic.txt geoipCN.txt apnic_cn.txt|uniq > cn.acl
+rm geoipCN.txt GeoIPCountryCSV.zip cnic.txt apnic.txt apnic_cn.txt
 rm -r GeoLite2-Country-CSV*
