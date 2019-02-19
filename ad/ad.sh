f58a41de10ae2d57eb674ed6dbe462887bd55c7e
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-02-19 18:53:06 +0800

    update

--- ad/ad.sh
+++ ad/ad.sh
@@ -1,8 +1,8 @@
-wget https://hosts.nfz.moe/full/hosts -O neohosts.acl
+#wget https://hosts.nfz.moe/full/hosts -O neohosts.acl
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O stevenhosts.acl
 wget "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" -O pglyoyo.acl
 
-cat pglyoyo.acl neohosts.acl stevenhosts.acl   | sed 's/^\#.*//g' | sed '/^ *$/d' | sed '/^$/d' | \
+cat pglyoyo.acl stevenhosts.acl   | sed 's/^\#.*//g' | sed '/^ *$/d' | sed '/^$/d' | \
 sed 's/\./\\\./g' | sed 's/$/\$/g' | sed 's/^\:\:1.*//g' | sed 's/^fe[0-9][0-9]\:\:.*//g' | \
 sed 's/^ff[0-9][0-9]\:\:.*//g' | sed 's/^255\.255\.255\.255.*//g' |sed 's/127\\.0\\.0\\.1 /(^|\\.)/' | \
 sed 's/0\\.0\\.0\\.0 /(^|\\.)/' | sed 's/\:\: /(^|\\.)/' | sed 's/\n\$/\$/' | \
@@ -35,5 +35,5 @@ uniq  > ad_temp.acl
 
 cat adstart.acl myad.acl  ad_temp.acl | sort -u | uniq > ad.acl
 cat adstart.acl myad.acl | sort -u | uniq> light_ad.acl
-rm ad_temp.acl pglyoyo.acl stevenhosts.acl neohosts.acl
+rm ad_temp.acl pglyoyo.acl stevenhosts.acl
 
